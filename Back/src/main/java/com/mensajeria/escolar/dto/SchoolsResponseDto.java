d6a2919c74b863c88460f6971c1edcac73c39653
RamonCruz187 <ramon.cruz187@gmail.com>
2024-07-16 21:27:11 -0300

    modify Schools Response

--- Back/src/main/java/com/mensajeria/escolar/dto/SchoolsResponseDto.java
+++ Back/src/main/java/com/mensajeria/escolar/dto/SchoolsResponseDto.java
@@ -14,12 +14,10 @@ import java.util.List;
 public class SchoolsResponseDto implements Serializable {
     public Long id;
     public String name;
-    public List<SchoolLevelResponseDto> levels;
 
     public SchoolsResponseDto(Escuela escuela) {
         this.id = escuela.getId();
         this.name = escuela.getNombre();
-        this.levels = escuela.getNiveles().stream().map(SchoolLevelResponseDto::new).toList();
     }
 
 }
